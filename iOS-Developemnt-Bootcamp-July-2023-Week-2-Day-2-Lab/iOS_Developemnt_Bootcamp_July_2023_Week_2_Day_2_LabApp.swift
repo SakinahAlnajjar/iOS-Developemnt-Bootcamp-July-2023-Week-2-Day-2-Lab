299049cdb40def5958287ff5cc55d33164896fcd
SakinahAlnajjar <sooka.1419@gmail.com>
2023-08-02 00:12:16 +0300

    Initial Commit

---
+++ iOS-Developemnt-Bootcamp-July-2023-Week-2-Day-2-Lab/iOS_Developemnt_Bootcamp_July_2023_Week_2_Day_2_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Developemnt_Bootcamp_July_2023_Week_2_Day_2_LabApp.swift
+//  iOS-Developemnt-Bootcamp-July-2023-Week-2-Day-2-Lab
+//
+//  Created by سكينه النجار on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Developemnt_Bootcamp_July_2023_Week_2_Day_2_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
